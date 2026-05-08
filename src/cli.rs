8ddbdec01dcd86b72b7b3b42cddcf8a5458ebaa2
Sam Vente <savente93@proton.me>
2026-05-08 11:53:21 +0200

    Initial commit

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+use clap_verbosity_flag::{LogLevel, Verbosity, VerbosityFilter};
+
+// don't have to construct it ourselves, tracing will just find it for us
+#[allow(dead_code)]
+pub struct CustomLogLevel {}
+
+impl LogLevel for CustomLogLevel {
+    fn default_filter() -> VerbosityFilter {
+        VerbosityFilter::Error
+    }
+    fn quiet_help() -> Option<&'static str> {
+        Some("suppress all logging output")
+    }
+    fn quiet_long_help() -> Option<&'static str> {
+        Some("Surpress the logging output of the application, including errors.")
+    }
+    fn verbose_help() -> Option<&'static str> {
+        Some("Increase verbosity of the logging (can be specified multiple times).")
+    }
+    fn verbose_long_help() -> Option<&'static str> {
+        Some(
+            "Increase the logging verbosity of the application by one level (ERROR, WARN, INFO, DEBUG, TRACE)",
+        )
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about= None)]
+pub struct Args {
+    #[command(flatten)]
+    pub verbose: Verbosity,
+}
