d102dc4b3cb5b9296aab53d98b31ef0d18950c9b
Sam Vente <savente93@proton.me>
2026-08-05 12:45:42 +0200

    fix(cli): remove unnecessary custom verbosity

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,35 +4,11 @@ pub(crate) mod suggest;
 pub(crate) mod write;
 
 use clap::{Parser, Subcommand};
-use clap_verbosity_flag::{LogLevel, Verbosity, VerbosityFilter};
+use clap_verbosity_flag::Verbosity;
 
 use crate::cli::source::DataSource;
 use crate::cli::suggest::SuggestArgs;
 use crate::cli::write::WriteArgs;
-
-// don't have to construct it ourselves, tracing will just find it for us
-#[allow(dead_code)]
-pub struct CustomLogLevel {}
-
-impl LogLevel for CustomLogLevel {
-    fn default_filter() -> VerbosityFilter {
-        VerbosityFilter::Error
-    }
-    fn quiet_help() -> Option<&'static str> {
-        Some("suppress all logging output")
-    }
-    fn quiet_long_help() -> Option<&'static str> {
-        Some("Suppress the logging output of the application, including errors.")
-    }
-    fn verbose_help() -> Option<&'static str> {
-        Some("Increase verbosity of the logging (can be specified multiple times).")
-    }
-    fn verbose_long_help() -> Option<&'static str> {
-        Some(
-            "Increase the logging verbosity of the application by one level (ERROR, WARN, INFO, DEBUG, TRACE)",
-        )
-    }
-}
 
 #[derive(Parser)]
 #[command(version, about, long_about= None)]
